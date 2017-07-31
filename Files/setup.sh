30a3bd97cb09388d0ccda7bfba6ad8a85ae19fe9
Apoooorv <appukhairnar92@gmail.com>
2017-07-31 16:08:37 -0400

    Changed the path of secret file

--- Files/setup.sh
+++ Files/setup.sh
@@ -6,8 +6,10 @@
 #cp /files/eng-rhel.repo /etc/yum.repos.d/
 #yum update -y
 #yum install -y resultsdb-updater
-cp /home/resultsdb-updater.py /etc/fedmsg.d/
+cp /home/resultsdb-updater.py /etc/resultsdb-updater/
 sed -i -e 's/validate_signatures=True/validate_signatures=False/g' /etc/fedmsg.d/ssl.py
-sed -i -e 's/{{ resultsdb_updater_user }}/'$RESULTSDB_USERNAME'/g' /etc/fedmsg.d/resultsdb-updater.py
-sed -i -e 's/{{ resultsdb_updater_password }}/'$RESULTSDB_PASSWORD'/g' /etc/fedmsg.d/resultsdb-updater.py
+sed -i -e 's/{{ resultsdb_updater_user }}/'$RESULTSDB_USERNAME'/g' /etc/resultsdb-updater/resultsdb-updater.py
+sed -i -e 's/{{ resultsdb_updater_password }}/'$RESULTSDB_PASSWORD'/g' /etc/resultsdb-updater/resultsdb-updater.py
+ln -s /etc/resultsdb-updater/resultsdb-updater.py /etc/fedmsg.d/resultsdb-update
+r.py
 fedmsg-hub
