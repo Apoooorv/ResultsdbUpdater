a945033e3866d78847546921a535f613d71c4cd5
Apoooorv <akhairna@redhat.com>
2017-07-31 14:07:57 -0400

    Added new testcases for fedrepo-req-branch

--- Files/setup.sh
+++ Files/setup.sh
@@ -1,12 +1,12 @@
-cp /files/Eng_Ops_CA.crt /etc/pki/ca-trust/source/anchors/
-cp /files/Red_Hat_IS_CA.crt /etc/pki/ca-trust/source/anchors/
-cp /files/Red_Hat_IT_Root_CA.crt /etc/pki/ca-trust/source/anchors/
-update-ca-trust
+#cp /files/Eng_Ops_CA.crt /etc/pki/ca-trust/source/anchors/
+#cp /files/Red_Hat_IS_CA.crt /etc/pki/ca-trust/source/anchors/
+#cp /files/Red_Hat_IT_Root_CA.crt /etc/pki/ca-trust/source/anchors/
+#update-ca-trust
 #yum install -y epel-release
 #cp /files/eng-rhel.repo /etc/yum.repos.d/
 #yum update -y
 #yum install -y resultsdb-updater
-cp /files/resultsdb-updater.py /etc/fedmsg.d/
+cp /home/resultsdb-updater.py /etc/fedmsg.d/
 sed -i -e 's/validate_signatures=True/validate_signatures=False/g' /etc/fedmsg.d/ssl.py
 sed -i -e 's/{{ resultsdb_updater_user }}/'$RESULTSDB_USERNAME'/g' /etc/fedmsg.d/resultsdb-updater.py
 sed -i -e 's/{{ resultsdb_updater_password }}/'$RESULTSDB_PASSWORD'/g' /etc/fedmsg.d/resultsdb-updater.py
