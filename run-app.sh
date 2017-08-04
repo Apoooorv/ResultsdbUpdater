3fbaeeccc6fc5a532b0039036cc6260e6b62041d
Hunor Csomortáni <csomh@redhat.com>
2017-08-04 11:44:04 +0200

    Remove sed for fedmsg.d/ssl.py
    I assume, that this should be overridden by the config for
    resultsdb-updater.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- run-app.sh
+++ run-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sed -i -e 's/validate_signatures=True/validate_signatures=False/g' /etc/fedmsg.d/ssl.py
+
+# Expect to find resultsdb-updater.py in a volume mounted
+# by OpenShift at /etc/resultsdb-updater
 ln -s /etc/resultsdb-updater/resultsdb-updater.py /etc/fedmsg.d/resultsdb-updater.py
 
 exec fedmsg-hub
