f4b1a4a760f0137b22412f947a559d399f5432b2
Hunor Csomortáni <csomh@redhat.com>
2017-08-04 10:38:20 +0200

    Improve wrapper script
    Just making it to be a propper bash script and
    using exec to start the hub.
    
    See <http://www.projectatomic.io/docs/docker-image-author-guidance/>.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 sed -i -e 's/validate_signatures=True/validate_signatures=False/g' /etc/fedmsg.d/ssl.py
 ln -s /etc/resultsdb-updater/resultsdb-updater.py /etc/fedmsg.d/resultsdb-updater.py
-fedmsg-hub
+
+exec fedmsg-hub
